14e369550f9df3af6ec1db6eccb62070dac083e5
Benjent <benjent@hotmail.fr>
2025-02-28 12:55:11 +0200

    feat: add conditions to exercises to improve guidance

--- app/page.tsx
+++ app/page.tsx
@@ -5,89 +5,146 @@ export default function Home() {
     {
       id: 1,
       description:
-        "Création d'un projet Next.js avec une page d'accueil et deux sous-pages partageant un layout avec un lien vers la page d'accueil.",
+        "Création d'un projet Next.js avec une page d'accueil et deux sous-pages.",
+      conditions: [
+        "Les sous-pages doivent partager un layout",
+        "Les sous-pages doivent avoir un lien vers la page d'accueil",
+      ],
       keywords: ["Next.js", "page"],
     },
     {
       id: 2,
       description:
         "Création d'une carte dont la couleur de fond change en fonction d'un input utilisateur.",
+      conditions: [
+        "L'input doit être réactif, c'est à dire que la couleur doit changer sans avoir à cliquer sur un bouton",
+      ],
       keywords: ["useState"],
     },
     {
       id: 3,
-      description:
-        "Enregistrer une liste de string dans un contexte et les modifier.",
+      description: "Enregistrer une liste de string dans un contexte.",
+      conditions: ["La liste doit être modifiable par un enfant du contexte"],
       keywords: ["useState", "useContext"],
     },
     {
       id: 4,
-      description: "Idem exercice 3, mais en passant par un state manager.",
+      description: "Idem exercice 3, mais en utilisant useReducer.",
+      conditions: ["La liste doit être modifiable par un enfant du contexte"],
       keywords: ["useContext", "useReducer"],
     },
     {
       id: 5,
       description: "Afficher une liste de formations créées depuis Baserow.",
+      conditions: [
+        "Créer un compte Baserow",
+        "Définir une base de données avec une table pour les formations",
+        "Une formation doit avoir à minima un nom et une date de début",
+        "Définir un database token et autoriser la lecture uniquement",
+      ],
       keywords: ["useQuery", "Baserow"],
     },
     {
       id: 6,
       description:
-        "Afficher une liste de formations créées depuis Baserow et les filtrer à partir d'une liste de tags créés aussi depuis Baserow.",
+        "Idem exercice 5, mais en rendant la liste filtrable à partir d'une liste de tags créés aussi depuis Baserow.",
+      conditions: [
+        "Créer un compte Baserow",
+        "Définir une base de données avec une table pour les formations et une autre pour les tags",
+        "Une formation doit avoir à minima un nom et une date de début ainsi qu'une liste de tags, un tag n'a qu'un nom",
+        "Le filtrage doit se faire à partir de checkboxes",
+      ],
       keywords: ["useQuery", "Baserow"],
     },
     {
       id: 7,
-      description:
-        "Se connecter en utilisant l'API de Baserow et stocker le résultat dans le session storage.",
+      description: "Se connecter en utilisant l'API de Baserow.",
+      conditions: [
+        "Créer un compte Baserow",
+        "Se connecter en fournissant un email et un mot de passe",
+        "Stocker le résultat dans le session storage",
+      ],
       keywords: ["useMutation", "Baserow"],
     },
     {
       id: 8,
       description:
         "En tant qu'utilisateur authentifié sur Baserow, afficher une liste de formations et les rendre supprimables.",
+      conditions: [
+        "Créer un compte Baserow",
+        "Se connecter en fournissant un email et un mot de passe",
+        "Afficher les formations en utilisant l'API générée par Baserow",
+        "Supprimer une formation en utilisant la session utilisateur",
+      ],
       keywords: ["useQuery", "useMutation", "Baserow"],
     },
     {
       id: 9,
       description:
         "En tant qu'utilisateur authentifié sur Baserow, ajouter une nouvelle formation.",
+      conditions: [
+        "Créer un compte Baserow",
+        "Se connecter en fournissant un email et un mot de passe",
+        "Créer un formulaire avec react-hook-form",
+        "Ajouter une formation en utilisant le formulaire et la session utilisateur",
+      ],
       keywords: ["useMutation", "Baserow", "react-hook-form", "axios"],
     },
     {
       id: 10,
       description:
         "En tant qu'utilisateur authentifié sur Baserow, éditer une formation.",
+      conditions: [
+        "Créer un compte Baserow",
+        "Se connecter en fournissant un email et un mot de passe",
+        "Créer un formulaire avec Formik",
+        "Editer une formation en utilisant le formulaire et la session utilisateur",
+      ],
       keywords: ["useMutation", "Baserow", "Formik"],
     },
     {
       id: 11,
       description:
         "Installer ESlint et Prettier et mettre en place le format-on-save sur son IDE.",
+      conditions: [
+        "Le formattage d'un fichier doit pouvoir se faire à chaque Ctrl+S",
+      ],
       keywords: ["ESlint", "Prettier"],
     },
     {
       id: 12,
       description:
-        "Créer votre site web perso en utilisant Next.js en SSG et le déployer en production (ou en URL temporaire) à l'aide de Vercel.",
+        "Créer votre site web perso en utilisant Next.js et le déployer en production (ou en URL temporaire) à l'aide de Vercel.",
+      conditions: [
+        "Le site doit être généré statiquement donc utiliser Next.js en SSG",
+      ],
       keywords: ["GitHub", "Vercel", "Tailwind"],
     },
     {
       id: 13,
       description:
-        "Utiliser Tailwind pour définir des classes pour thème sombre et permettre le changement entre thèmes clair et sombre.",
+        "Utiliser Tailwind pour définir des classes pour thème sombre.",
+      conditions: [
+        "Le thème doit pouvoir alterner entre clair et sombre au clic d'un bouton",
+      ],
       keywords: ["Tailwind"],
     },
     {
       id: 14,
       description:
         "Utiliser TypeScript pour typer le modèle de vos tables Baserow ainsi que le résultat des appels API Baserow.",
+      conditions: [
+        "Limiter le copier-coller : les types (ou interfaces) doivent pouvoir étendre d'autres types (ou interfaces)",
+        "Un type doit pouvoir accepter un autre type en paramètre",
+      ],
       keywords: ["TS", "Baserow"],
     },
     {
       id: 15,
-      description:
-        "Mettre en place une suite de tests unitaires et l'exécuter via un script de package.json.",
+      description: "Mettre en place une suite de tests unitaires.",
+      conditions: [
+        "Les tests doivent pouvoir se lancer via une commande npm run test",
+      ],
       keywords: ["Vitest"],
     },
   ];
@@ -106,6 +163,9 @@ export default function Home() {
           >
             <h2 className="text-4xl">Exercice {exercice.id}</h2>
             <p className="">{exercice.description}</p>
+            <ul className="list-inside list-disc text-base italic">
+              {exercice.conditions?.map((c) => <li key={c}>{c}</li>)}
+            </ul>
             <div>
               <p className="mb-2">Outils requis pour l&apos;exercice&nbsp;:</p>
               <ul className="flex flex-wrap gap-2 text-sm uppercase">
