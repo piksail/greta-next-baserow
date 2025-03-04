a474b98aef1462e56462e74e7aa789938d0f2309
Benjent <benjent@hotmail.fr>
2025-03-04 11:33:21 +0200

    feat: allow exo15 to use datatestid

--- app/exo-15/page.test.tsx
+++ app/exo-15/page.test.tsx
@@ -2,10 +2,16 @@ import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Page from "./page";
 
-test("Page", () => {
+test("Vérifie que la page rende bien un h1 et un retour vers la page d'accueil", () => {
   render(<Page />);
+
+  // Approche "humaine/utilisateur"
   expect(
     screen.getByRole("heading", { level: 1, name: "Exercice 15" }),
   ).toBeDefined();
   expect(screen.getByRole("link", { name: "Aller à l'accueil" })).toBeDefined();
+
+  // Approche "technique"
+  expect(screen.getByTestId("mainHeading")).toBeDefined();
+  expect(screen.getByTestId("backToHome")).toBeDefined();
 });
--- app/exo-15/page.tsx
+++ app/exo-15/page.tsx
@@ -5,8 +5,11 @@ import Link from "next/link";
 export default function Exo15() {
   return (
     <main className="mx-auto max-w-7xl p-12 text-center">
-      <h1 className="text-7xl font-bold">Exercice 15</h1>
+      <h1 className="text-7xl font-bold" data-testid="mainHeading">
+        Exercice 15
+      </h1>
       <Link
+        data-testid="backToHome"
         href="/"
         className="mt-12 inline-block rounded bg-indigo-500 px-4 py-2 text-2xl font-bold text-white"
       >
